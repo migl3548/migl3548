2f76a2f7594e0554b80dfa9f0204101c6ced48aa
ari <migl3548@colorado.edu>
2024-11-12 10:41:50 -0700

    finished ica12

---
+++ ica/ica12/ica12.js
@@ -0,0 +1,27 @@
+const p = document.getElementById('text');
+const callButton = document.getElementById("call");
+
+function fetchData(){
+    //in class example 
+    fetch('https://catfact.ninja/fact')
+        .then(res => {
+            if(res.ok){
+                return res.json();
+            }else{
+                console.log(res);
+            }
+        }).then(res=> {
+            const myData = res.fact;
+            console.log(myData);
+            p.innerHTML = myData;
+        })
+        .catch(error => {console.log(error)})
+}
+// fetchData();
+
+
+document.getElementById("call").addEventListener("click", e => { fetchData(); });    
+console.log(myData);
+
+// p.innerHTML = myData;
+
