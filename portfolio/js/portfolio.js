d35ed5bf2fd98517f13de80267b9bf0423915a75
ari <migl3548@colorado.edu>
2024-12-09 14:42:02 -0700

    more js

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -17,18 +17,33 @@ fetch('../../portfolio/projects.json')
 //     }
 // }
 
+// function parseData(data) {
+//     for (let i = 0; i < data.projects.length; i++) {
+//         document.getElementById("projects").innerHTML += `
+//             <div class="row project" id="${data.projects[i].subdomain}">
+//                 <div class="projimg">
+//                     <img src="../../portfolio/images/img(${i + 1}).png" alt="${data.projects[i].name}">
+//                 </div>
+//                 <div class="description">
+//                     <h2>${data.projects[i].name}</h2>
+//                     <p class="subtitle">${data.projects[i].subtitle}</p>
+//                 </div>
+//                 <p>${data.projects[i].abstract}</p>
+//             </div>`;
+//     }
+// }
 function parseData(data) {
     for (let i = 0; i < data.projects.length; i++) {
         document.getElementById("projects").innerHTML += `
-            <div class="row project" id="${data.projects[i].subdomain}">
+            <div class="row project" id="${data.projects[i].subdomain || ''}">
                 <div class="projimg">
-                    <img src="images/ex/ex (${i + 1}).png" alt="${data.projects[i].name}">
+                    <img src="../../portfolio/images/img(${i + 1}).png" alt="${data.projects[i].name}">
                 </div>
                 <div class="description">
                     <h2>${data.projects[i].name}</h2>
-                    <p class="subtitle">${data.projects[i].subtitle}</p>
+                    <p class="subtitle">${data.projects[i].subtitle || 'Subtitle not available'}</p>
+                    <p class="abstract">${data.projects[i].abstract || 'Abstract not available'}</p>
                 </div>
-                <p>${data.projects[i].abstract}</p>
             </div>`;
     }
 }
