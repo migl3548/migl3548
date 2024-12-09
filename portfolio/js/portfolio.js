84b75a31fd5f9b9c571c2f6eacf69b13e3959f63
ari <migl3548@colorado.edu>
2024-12-09 13:35:40 -0700

    got some of the js working

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -1,13 +1,36 @@
-fetch('../projects.json')
-.then(function(response){
-    return response.json();
-}).then(projects => {
-    console.log(projects);
-    parseData(projects);
-}).catch(err =>{
-    console.log(`error ${err}`);
-})
+fetch('../../portfolio/projects.json')
+    .then(response=>{
+        return response.json();
+    }).then(projects => {
+        console.log(projects);
+        parseData(projects);
+    }).catch(err =>{
+        console.log(`error ${err}`);
+    })
 
-function parseData(data){
-    
+// function parseData(data){
+//     for(let i=0; i<data.projects.length; i++){
+//         document.getElementById("projects").innerHTML += `<div class="row project" id="${data.projects[i].subdomain}">`
+//             <div class="projimg"><img src="images/ex/ex (${i+1}).png"></div>
+//             <div class="description"><h2>${projects.projects[i].name}</h2><p class = "subtitle">${data.projects[i].subtitle}</p></div>
+//             <p>${data.projects[i].abstract}</p></div></div>`;
+//     }
+// }
+
+function parseData(data) {
+    for (let i = 0; i < data.projects.length; i++) {
+        document.getElementById("projects").innerHTML += `
+            <div class="row project" id="${data.projects[i].subdomain}">
+                <div class="projimg">
+                    <img src="images/ex/ex (${i + 1}).png" alt="${data.projects[i].name}">
+                </div>
+                <div class="description">
+                    <h2>${data.projects[i].name}</h2>
+                    <p class="subtitle">${data.projects[i].subtitle}</p>
+                </div>
+                <p>${data.projects[i].abstract}</p>
+            </div>`;
+    }
 }
+
+console.log("hello");
